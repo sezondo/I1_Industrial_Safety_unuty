ed1e4179a9eb87d9245edfe26bf13c7849cab8fb
sezondo <psdd205050@gmail.com>
2025-07-04 11:39:38 +0900

    기본 환경 조성

---
+++ program/I1/Assets/Script/Track.cs
@@ -0,0 +1,97 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARFoundation.VisualScripting;
+ 
+public class Track : MonoBehaviour
+{
+    public ARTrackedImageManager manager;
+    public List<GameObject> list1 = new List<GameObject>();
+    private Dictionary<string, GameObject> dict1
+                                    = new Dictionary<string, GameObject>();
+ 
+    public List<AudioClip> list2 = new List<AudioClip>();
+    private Dictionary<string, AudioClip> dict2
+                            = new Dictionary<string, AudioClip>();      
+
+
+    public List<AudioClip> list3 = new List<AudioClip>();
+    private Dictionary<string, AudioClip> dict3
+                            = new Dictionary<string, AudioClip>();
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        foreach (GameObject o in list1)
+        {
+            dict1.Add(o.name, o);
+        }
+
+        foreach (AudioClip o in list2)
+        {
+            dict2.Add(o.name, o);
+        }
+        foreach (AudioClip o in list2)
+        {
+            dict3.Add(o.name, o);
+        }
+ 
+    }
+
+    void UpdateImage(ARTrackedImage t)
+    {
+        string name = t.referenceImage.name;
+ 
+        GameObject o = dict1[name];
+        o.transform.position = t.transform.position;
+        o.transform.rotation = t.transform.rotation;
+        o.SetActive(true);
+    }
+ 
+    void UpdateSound(ARTrackedImage t)
+    {
+        string name = t.referenceImage.name;
+ 
+        AudioClip o = dict2[name];
+        GetComponent<AudioSource>().PlayOneShot(o);
+    }
+ 
+ 
+    void OnChanged(ARTrackablesChangedEventArgs<ARTrackedImage> eventArgs)
+    {
+        foreach (var t in eventArgs.added)
+        {
+            // Handle added event
+            UpdateImage(t);
+            UpdateSound(t);
+        }
+ 
+        foreach (var t in eventArgs.updated)
+        {
+            // Handle updated event
+            UpdateImage(t);
+        }
+ 
+        foreach (var removedImage in eventArgs.removed)
+        {
+            // Handle removed event
+        }
+    }
+ 
+ 
+ 
+    void OnEnable() => manager.trackablesChanged.AddListener(OnChanged);
+ 
+    void OnDisable() => manager.trackablesChanged.RemoveListener(OnChanged);
+ 
+ 
+ 
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
